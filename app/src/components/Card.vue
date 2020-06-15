9cb2d873876c959b9e3997a71db3698e0f7b5bfd
amj311 <amjudd315@gmail.com>
2020-06-15 07:39:28 -0600

    sorted half of words. Created demo card layout with vue ehich randomizes words and flips cards

---
+++ app/src/components/Card.vue
@@ -0,0 +1,47 @@
+<template>
+  <div v-on:click="this.flipCard" class="wrapper" :class="{flipped: flipped}">
+    <div>{{ word }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    word: String
+  },
+
+  data() { return {
+    flipped: false
+  }},
+
+  methods: {
+    flipCard: function() {
+      console.log('flipped')
+      this.flipped = true;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.wrapper {
+  border-radius: 10px;
+  border: .25em solid;
+  padding: 1em;
+  height: 5em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  user-select: none;
+  cursor: pointer;
+  perspective: 1000px;
+}
+
+.wrapper.flipped {
+  transform: rotateX(180deg);
+  transition: 500ms;
+  transform-style: preserve-3d;
+}
+</style>
---
+++ app/src/components/HelloWorld.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div class="cards-table">
+      <div v-for="word in words" :key="word" class="card-cell">
+        <Card :word="word" />
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import Card from './Card.vue'
+import wordSet from '../assets/words/test.json'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  components: {
+    Card
+  },
+
+  data() { return {
+    words: [],
+  }},
+
+  created() {
+    console.log(wordSet.words.length);
+    for (let i = 0; i < 25; i ++) {
+      let idx = Math.floor(Math.random()*wordSet.words.length);
+      this.words.push(wordSet.words[idx])
+    }
+  },
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.cards-table {
+  display: flex;
+  flex-wrap: wrap;
+}
+.card-cell {
+  box-sizing: border-box;
+  min-width: 20%;
+  max-width: 20%;
+  padding: .5em;
+}
+</style>
