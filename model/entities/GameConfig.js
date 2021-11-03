f1ef40f6c2dbd0570f28910e11d0169b949b5624
Arthur Judd <amjudd315@gmail.com>
2021-11-03 08:27:27 -0600

    moved folders inside of vue-app

--- model/entities/GameConfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = class GameConfig {
-    constructor(props) {
-        this.numCardsSqrt = props.numCardsSqrt;
-        this.numTeams = props.numTeams;
-        this.numTeamCards = props.numTeamCards;
-        this.numAssassins = props.numAssassins;
-        this.numBystanders = props.numBystanders;
-    }
-}
